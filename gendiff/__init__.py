5e3bf9cdf28aa98dc1789c3ee28527657f2caec3
Sergei <reyangood@gmail.com>
2023-04-03 02:59:06 +0300

    splitting the check_key function into two and adding a check for boolean values

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,28 +1,38 @@
-import json
+from gendiff.scripts.parsing import parse_files
+
+
+def bool_lower(key, file):
+    if isinstance(file[key], bool):
+        return str(file[key]).lower()
+    else:
+        return file[key]
+
+
+def check_same_key(result, key, f1, f2):
+    if f1[key] == f2[key]:
+        result += f"    {key}: {bool_lower(key, f1)}\n"
+    else:
+        result += f"  - {key}: {bool_lower(key, f1)}\n"
+        result += f"  + {key}: {bool_lower(key, f2)}\n"
+    return result
 
 
 def check_key(result, key, f1, f2):
     if key in f1 and key in f2:
-        if f1[key] == f2[key]:
-            result += f"    {key}: {f1[key]}\n"
-        else:
-            result += f"  - {key}: {f1[key]}\n"
-            result += f"  + {key}: {f2[key]}\n"
+        result = check_same_key(result, key, f1, f2)
     elif key in f1 and key not in f2:
-        result += f"  - {key}: {f1[key]}\n"
+        result += f"  - {key}: {bool_lower(key, f1)}\n"
     elif key not in f1 and key in f2:
-        result += f"  + {key}: {f2[key]}\n"
+        result += f"  + {key}: {bool_lower(key, f2)}\n"
     return result
 
 
 def generate_diff(file_path1: str, file_path2: str) -> str:
     result = "{\n"
-    with open(file_path1) as f1, open(file_path2) as f2:
-        f1 = json.load(f1)
-        f2 = json.load(f2)
-        overall = set(f1) | set(f2)
-        overall_sorted = sorted(overall)
-        for key in overall_sorted:
-            result = check_key(result, key, f1, f2)
+    f1, f2 = parse_files(file_path1, file_path2)
+    overall = set(f1) | set(f2)
+    overall_sorted = sorted(overall)
+    for key in overall_sorted:
+        result = check_key(result, key, f1, f2)
     result += '}'
     return result

457631363e52d212f097ceef61efacea9d9f29dd
Sergei <reyangood@gmail.com>
2023-04-15 23:38:55 +0300

    add new fixtures for recursive tests

--- tests/fixtures/test_files.py
+++ tests/fixtures/test_files.py
@@ -21,5 +21,25 @@ def file1_yaml():
 
 
 @pytest.fixture
-def file2_yaml():
+def file2_yml():
     return "tests/fixtures/file2.yml"
+
+
+@pytest.fixture
+def big_file1_json():
+    return "tests/fixtures/big_file1.json"
+
+
+@pytest.fixture
+def big_file2_json():
+    return "tests/fixtures/big_file2.json"
+
+
+@pytest.fixture
+def big_file1_yaml():
+    return "tests/fixtures/big_file1.yaml"
+
+
+@pytest.fixture
+def big_file2_yml():
+    return "tests/fixtures/big_file2.yml"

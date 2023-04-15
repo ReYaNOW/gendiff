a0ae31a1c901e826a4d86401591e65a15c9651eb
Sergei <reyangood@gmail.com>
2023-04-15 23:36:18 +0300

    change __init__.py and add stylish.py to make it work recursively

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,38 +1,24 @@
-from gendiff.scripts.parsing import parse_files
+from gendiff.parsing import parse_files
+from gendiff.diff import make_diff
+from gendiff.stylish import out_stylish
+from copy import deepcopy
 
 
-def bool_lower(key, file):
-    if isinstance(file[key], bool):
-        return str(file[key]).lower()
-    else:
-        return file[key]
+def update(f1, f2):
+    for k, v in f2.items():
+        if isinstance(v, dict):
+            f1[k] = update(f1.get(k, {}), v)
+        else:
+            f1[k] = v
+    return f1
 
 
-def check_same_key(result, key, f1, f2):
-    if f1[key] == f2[key]:
-        result += f"    {key}: {bool_lower(key, f1)}\n"
-    else:
-        result += f"  - {key}: {bool_lower(key, f1)}\n"
-        result += f"  + {key}: {bool_lower(key, f2)}\n"
-    return result
-
-
-def check_key(result, key, f1, f2):
-    if key in f1 and key in f2:
-        result = check_same_key(result, key, f1, f2)
-    elif key in f1 and key not in f2:
-        result += f"  - {key}: {bool_lower(key, f1)}\n"
-    elif key not in f1 and key in f2:
-        result += f"  + {key}: {bool_lower(key, f2)}\n"
-    return result
-
-
-def generate_diff(file_path1: str, file_path2: str) -> str:
-    result = "{\n"
-    f1, f2 = parse_files(file_path1, file_path2)
-    overall = set(f1) | set(f2)
-    overall_sorted = sorted(overall)
-    for key in overall_sorted:
-        result = check_key(result, key, f1, f2)
-    result += '}'
-    return result
+def generate_diff(path1: str, path2: str, format: str = "stylish") -> str:
+    f1, f2 = parse_files(path1, path2)
+    overall = update(deepcopy(f1), deepcopy(f2))
+    diff = make_diff(overall, f1, f2)
+    match format:
+        case "stylish":
+            return out_stylish(diff)
+        case _:
+            return "Wrong format!"

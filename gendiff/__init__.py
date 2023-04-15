678e9296b78446addb0363f5a7a30110fe9665bd
Sergei <reyangood@gmail.com>
2023-04-16 02:20:23 +0300

    add plain output format

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,7 @@
 from gendiff.parsing import parse_files
 from gendiff.diff import make_diff
 from gendiff.stylish import out_stylish
+from gendiff.plain import out_plain
 from copy import deepcopy
 
 
@@ -13,12 +14,14 @@ def update(f1, f2):
     return f1
 
 
-def generate_diff(path1: str, path2: str, format: str = "stylish") -> str:
+def generate_diff(path1: str, path2: str, format_name: str = "stylish") -> str:
     f1, f2 = parse_files(path1, path2)
     overall = update(deepcopy(f1), deepcopy(f2))
     diff = make_diff(overall, f1, f2)
-    match format:
+    match format_name:
         case "stylish":
             return out_stylish(diff)
+        case "plain":
+            return out_plain(diff)
         case _:
-            return "Wrong format!"
+            return "Wrong format!\nPlease choose from stylish, plain or json"

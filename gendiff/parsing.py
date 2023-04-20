7600c0b3214d5b8fb721f74668aa66ec515996f7
Sergei <reyangood@gmail.com>
2023-04-20 13:40:38 +0300

    add docstring to parse_files in parsing.py

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -13,6 +13,16 @@ def parser(file, extension):
 
 
 def parse_files(file_path1: str, file_path2: str) -> tuple:
+    """
+    This function parses two files and returns their contents.
+
+    Args:
+    - file_path1 (str): the path to the first file to be parsed.
+    - file_path2 (str): the path to the second file to be parsed.
+
+    Returns:
+    - tuple: a tuple containing the contents of the two files.
+    """
     f1 = parser(file_path1, file_path1.split(".")[-1])
     f2 = parser(file_path2, file_path2.split(".")[-1])
     return f1, f2

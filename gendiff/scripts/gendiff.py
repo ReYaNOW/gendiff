eafe12579181c9612449628115c701e469df2a8f
Sergei <reyangood@gmail.com>
2023-11-01 06:08:17 +0300

    code refactoring

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,9 @@
 from gendiff import generate_diff
-from gendiff.cli import get_args_from_cli
+from gendiff.cli import parse_args_from_cli
 
 
 def main():
-    args = get_args_from_cli()
+    args = parse_args_from_cli()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 

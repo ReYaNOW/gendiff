cb42d632eab12ccb229abb412c84576d252adc0f
Sergei <reyangood@gmail.com>
2023-11-18 09:53:32 +0300

    code refactoring in plain.py

--- gendiff/output_formats/plain.py
+++ gendiff/output_formats/plain.py
@@ -3,36 +3,32 @@ from gendiff.utils import validate_value
 FORMAT_NAME = 'plain'
 
 
-def validate_path(path: str, key) -> str:
-    if path == '':
-        return key
-    return f'{path}.{key}'
-
-
-def plain(diff: dict, current_path: str = '') -> list:
+def plain(diff: dict, curr_prop_path: str = '') -> list:
     result = []
     for key, v_info in diff.items():
         type_ = v_info['type']
         value = v_info['value']
-        path = validate_path(current_path, key)
+        property_name = f'{curr_prop_path}{key}'
 
         match type_:
             case 'nested':
-                result.extend(plain(value, path))
+                result.extend(plain(value, f'{property_name}.'))
             case 'added':
                 val = validate_value(value, FORMAT_NAME)
-                result.append(f"Property '{path}' was added with value: {val}")
+                result.append(
+                    f"Property '{property_name}' was added with value: {val}"
+                )
             case 'deleted':
-                result.append(f"Property '{path}' was removed")
+                result.append(f"Property '{property_name}' was removed")
             case 'changed':
                 old_val = validate_value(value, FORMAT_NAME)
                 new_val = validate_value(v_info['new_value'], FORMAT_NAME)
                 result.append(
-                    f"Property '{path}' was updated. "
+                    f"Property '{property_name}' was updated. "
                     f"From {old_val} to {new_val}"
                 )
     return result
 
 
-def out_plain(diff: dict) -> str:
+def render_plain(diff: dict) -> str:
     return '\n'.join(plain(diff))

e736013e37a46d5c9b918143bd5c324839e29777
Sergei <reyangood@gmail.com>
2023-11-18 09:53:42 +0300

    code refactoring in stylish.py

--- gendiff/output_formats/stylish.py
+++ gendiff/output_formats/stylish.py
@@ -1,9 +1,14 @@
+import itertools
+
 from gendiff.utils import validate_value
 
 INTEND = '    '
 
 
-def get_lines_from_node(key: str, value, depth: int, type_: str) -> list:
+def get_line(key: str, value, depth, type_=None) -> str:
+    new_depth = depth + 1
+    deep_indent = INTEND * new_depth
+
     match type_:
         case 'added':
             symbol = '+'
@@ -12,55 +17,33 @@ def get_lines_from_node(key: str, value, depth: int, type_: str) -> list:
         case _:
             symbol = ' '
 
-    if isinstance(value, dict):
-        new_lines = [get_line_with_brace(depth, key, symbol, opening=True)]
-        new_lines.extend(add_dict_without_internal_changes(value, depth + 1))
-        new_lines.append(get_line_with_brace(depth, opening=False))
-        return new_lines
-    return [f'{(INTEND * depth)[:-2]}{symbol} {key}: {validate_value(value)}']
+    return f'{deep_indent[:-2]}{symbol} {key}: {stylish(value, new_depth)}'
 
 
-def add_dict_without_internal_changes(value: dict, depth: int) -> list:
-    result = []
-    for k, v in value.items():
-        if isinstance(v, dict):
-            result.append(get_line_with_brace(depth, k, opening=True))
-            result.extend(add_dict_without_internal_changes(v, depth + 1))
-            result.append(get_line_with_brace(depth, opening=False))
-        else:
-            result.append(f'{INTEND * depth}{k}: {validate_value(v)}')
-    return result
+def stylish(current_value, depth):
+    if not isinstance(current_value, dict):
+        return validate_value(current_value)
 
+    lines = []
+    for key, key_info in current_value.items():
+        if not isinstance(key_info, dict) or 'type' not in key_info:
+            lines.append(get_line(key, key_info, depth))
+            continue
 
-def get_line_with_brace(depth: int, key=None, symbol='', opening=True) -> str:
-    if opening:
-        if symbol:
-            return f'{(INTEND * depth)[:-2]}{symbol} {key}: ' + '{'
-        return f'{INTEND * depth}{key}: ' + '{'
-    return f'{INTEND * depth}' + '}'
-
-
-def stylish(diff: dict, depth=1):
-    result = []
-    for key, key_info in diff.items():
         type_ = key_info['type']
         val = key_info['value']
         new_val = key_info.get('new_value')
 
-        match type_:
-            case 'nested':
-                result.append(get_line_with_brace(depth, key, opening=True))
-                result.extend(stylish(val, depth + 1))
-                result.append(get_line_with_brace(depth, opening=False))
-            case 'changed':
-                result.extend(get_lines_from_node(key, val, depth, 'deleted'))
-                result.extend(
-                    get_lines_from_node(key, new_val, depth, 'added')
-                )
-            case _:
-                result.extend(get_lines_from_node(key, val, depth, type_))
-    return result
+        if type_ == 'changed':
+            lines.append(get_line(key, val, depth, type_='deleted'))
+            lines.append(get_line(key, new_val, depth, type_='added'))
+        else:
+            lines.append(get_line(key, val, depth, type_))
+
+    current_indent = '    ' * depth
+    result = itertools.chain("{", lines, [current_indent + "}"])
+    return '\n'.join(result)
 
 
-def out_stylish(diff: dict):
-    return "{\n" + "\n".join(stylish(diff)) + '\n}'
+def render_stylish(diff: dict):
+    return stylish(diff, depth=0)

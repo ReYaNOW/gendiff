cf0968c2e9809643e4cf50b977b8ab3d5bb6e1f8
Sergei <reyangood@gmail.com>
2023-11-18 09:52:38 +0300

    code refactoring in diff_tree.py

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -16,9 +16,9 @@ def make_diff_tree(dict1: dict, dict2: dict) -> dict:
     return diff
 
 
-def get_diff_with_same_keys(dict1, dict2):
-    if isinstance(dict1, dict) and isinstance(dict2, dict):
-        return {'type': 'nested', 'value': make_diff_tree(dict1, dict2)}
-    if dict1 == dict2:
-        return {'type': 'kept', 'value': dict1}
-    return {'type': 'changed', 'value': dict1, 'new_value': dict2}
+def get_diff_with_same_keys(val1, val2):
+    if isinstance(val1, dict) and isinstance(val2, dict):
+        return {'type': 'nested', 'value': make_diff_tree(val1, val2)}
+    if val1 == val2:
+        return {'type': 'kept', 'value': val1}
+    return {'type': 'changed', 'value': val1, 'new_value': val2}

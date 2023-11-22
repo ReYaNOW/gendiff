4fe37eb12bf6b315317be6af161749ca5da14989
Sergei <reyangood@gmail.com>
2023-11-22 10:54:49 +0300

    code refactoring in output_formats/__init__.py

--- gendiff/output_formats/__init__.py
+++ gendiff/output_formats/__init__.py
@@ -3,13 +3,13 @@ from gendiff.output_formats.plain import render_plain
 from gendiff.output_formats.stylish import render_stylish
 
 
-def generate_output(diff: dict, format_name: str) -> str:
+def generate_output(diff_tree: dict, format_name: str) -> str:
     match format_name:
         case 'stylish':
-            return render_stylish(diff)
+            return render_stylish(diff_tree)
         case 'plain':
-            return render_plain(diff)
+            return render_plain(diff_tree)
         case 'json':
-            return render_json(diff)
+            return render_json(diff_tree)
         case _:
             return 'Wrong format!\nPlease choose from stylish, plain or json'

debb4d827eac2bb028ea565faf47293f27fb5de9
Sergei <reyangood@gmail.com>
2023-11-22 16:51:31 +0300

    change output when input wrong format

--- gendiff/output_formats/__init__.py
+++ gendiff/output_formats/__init__.py
@@ -1,15 +1,22 @@
+from collections import namedtuple
+
 from gendiff.output_formats.json import render_json
 from gendiff.output_formats.plain import render_plain
 from gendiff.output_formats.stylish import render_stylish
 
+_FORMAT_VALUES = ('stylish', 'plain', 'json')
+FORMATS = namedtuple('FormatChoices', map(str.upper, _FORMAT_VALUES))(
+    *_FORMAT_VALUES
+)
+
 
 def generate_output(diff_tree: dict, format_name: str) -> str:
     match format_name:
-        case 'stylish':
+        case FORMATS.STYLISH:
             return render_stylish(diff_tree)
-        case 'plain':
+        case FORMATS.PLAIN:
             return render_plain(diff_tree)
-        case 'json':
+        case FORMATS.JSON:
             return render_json(diff_tree)
         case _:
-            return 'Wrong format!\nPlease choose from stylish, plain or json'
+            return f'Wrong format!\nPlease choose from {", ".join(FORMATS)}'

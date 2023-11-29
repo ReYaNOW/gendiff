dc10a600548f9b758d8ec157b11daeaa8d1bde64
Sergei <reyangood@gmail.com>
2023-11-29 23:22:09 +0300

    code refactoring in stylish.py

--- gendiff/output_formats/stylish.py
+++ gendiff/output_formats/stylish.py
@@ -18,29 +18,28 @@ def render_stylish(diff: dict):
 
         lines = []
         for k, key_info in current_value.items():
-            match key_info:
-                case {'type': 'added', 'value': add_v}:
-                    lines.append(generate_line(k, add_v, depth, symbol='+'))
+            val = key_info['value']
+            new_v = key_info.get('new_value')
 
-                case {'type': 'deleted', 'value': del_v}:
-                    lines.append(generate_line(k, del_v, depth, symbol='-'))
+            match key_info['type']:
+                case 'added':
+                    lines.append(generate_line(k, val, depth, symbol='+'))
 
-                case {'type': 'changed', 'value': old_v, 'new_value': new_v}:
-                    lines.append(generate_line(k, old_v, depth, symbol='-'))
+                case 'deleted':
+                    lines.append(generate_line(k, val, depth, symbol='-'))
+
+                case 'changed':
+                    lines.append(generate_line(k, val, depth, symbol='-'))
                     lines.append(generate_line(k, new_v, depth, symbol='+'))
 
-                case {'type': 'nested'} | {'type': 'unchanged'}:
-                    value = key_info['value']
-                    lines.append(generate_line(k, value, depth, symbol=' '))
-
                 case _:
-                    lines.append(generate_line(k, key_info, depth, symbol=' '))
+                    lines.append(generate_line(k, val, depth, symbol=' '))
 
         closing_bracket = f'{INDENT * depth}}}'
         lines = '\n'.join(lines)
         return f'{{\n{lines}\n{closing_bracket}'
 
-    def generate_line(key, value, depth, symbol):
+    def generate_line(key: str, value, depth: int, symbol: str):
         new_depth = depth + 1
         deep_indent = (INDENT * new_depth)[:-2]
         return f'{deep_indent}{symbol} {key}: {stylish(value, new_depth)}'

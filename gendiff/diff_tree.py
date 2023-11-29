75c9516c0083401a314792a62ab593f6f2a6de62
Sergei <reyangood@gmail.com>
2023-11-29 03:00:20 +0300

    fix bug with nested dicts in source file in diff_tree.py

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -10,9 +10,9 @@ def build_diff_tree(dict1: dict, dict2: dict) -> dict:
         if key in dict1 and key in dict2:
             diff[key] = get_diff_with_same_keys(dict1[key], dict2[key])
         elif key not in dict1:
-            diff[key] = {'type': 'added', 'value': dict2[key]}
+            diff[key] = get_diff_with_not_same_keys(dict2[key], 'added')
         else:
-            diff[key] = {'type': 'deleted', 'value': dict1[key]}
+            diff[key] = get_diff_with_not_same_keys(dict1[key], 'deleted')
     return diff
 
 
@@ -22,3 +22,9 @@ def get_diff_with_same_keys(val1, val2):
     if val1 == val2:
         return {'type': 'unchanged', 'value': val1}
     return {'type': 'changed', 'value': val1, 'new_value': val2}
+
+
+def get_diff_with_not_same_keys(value, type_):
+    if isinstance(value, dict):
+        return {'type': type_, 'value': build_diff_tree(value, value)}
+    return {'type': type_, 'value': value}

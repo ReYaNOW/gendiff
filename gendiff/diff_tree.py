8752af124ae8ac55e7878ddee7f01f36d7e1cab6
Sergei <reyangood@gmail.com>
2023-11-29 22:59:40 +0300

    fix bug with str values in diff_tree.py

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -10,9 +10,9 @@ def build_diff_tree(dict1: dict, dict2: dict) -> dict:
         if key in dict1 and key in dict2:
             diff[key] = get_diff_with_same_keys(dict1[key], dict2[key])
         elif key not in dict1:
-            diff[key] = get_diff_with_not_same_keys(dict2[key], 'added')
+            diff[key] = {'type': 'added', 'value': validate_val(dict2[key])}
         else:
-            diff[key] = get_diff_with_not_same_keys(dict1[key], 'deleted')
+            diff[key] = {'type': 'deleted', 'value': validate_val(dict1[key])}
     return diff
 
 
@@ -20,11 +20,13 @@ def get_diff_with_same_keys(val1, val2):
     if isinstance(val1, dict) and isinstance(val2, dict):
         return {'type': 'nested', 'value': build_diff_tree(val1, val2)}
     if val1 == val2:
-        return {'type': 'unchanged', 'value': val1}
+        return {'type': 'unchanged', 'value': validate_val(val1)}
+
+    val1, val2 = validate_val(val1), validate_val(val2)
     return {'type': 'changed', 'value': val1, 'new_value': val2}
 
 
-def get_diff_with_not_same_keys(value, type_):
+def validate_val(value):
     if isinstance(value, dict):
-        return {'type': type_, 'value': build_diff_tree(value, value)}
-    return {'type': type_, 'value': value}
+        return build_diff_tree(value, value)
+    return value

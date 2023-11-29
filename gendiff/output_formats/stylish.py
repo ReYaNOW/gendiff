c933ac2a62c4f747f3d719161e4b389ea8af3a6c
Sergei <reyangood@gmail.com>
2023-11-29 03:00:39 +0300

    code refactoring in stylish.py

--- gendiff/output_formats/stylish.py
+++ gendiff/output_formats/stylish.py
@@ -11,49 +11,38 @@ def stringify_value(value) -> str:
             return f'{value}'
 
 
-def validate_key_info(key_info):
-    if not isinstance(key_info, dict) or 'type' not in key_info:
-        return False
-    return True
-
-
 def render_stylish(diff: dict):
     def stylish(current_value, depth: int) -> str:
         if not isinstance(current_value, dict):
             return stringify_value(current_value)
 
         lines = []
-        for key, key_info in current_value.items():
-            if not validate_key_info(key_info):
-                lines.append(generate_line(key, key_info, depth))
-                continue
+        for k, key_info in current_value.items():
+            match key_info:
+                case {'type': 'added', 'value': add_v}:
+                    lines.append(generate_line(k, add_v, depth, symbol='+'))
 
-            type_ = key_info['type']
-            val = key_info['value']
-            new_val = key_info.get('new_value')
+                case {'type': 'deleted', 'value': del_v}:
+                    lines.append(generate_line(k, del_v, depth, symbol='-'))
 
-            if type_ == 'changed':
-                lines.append(generate_line(key, val, depth, type_='deleted'))
-                lines.append(generate_line(key, new_val, depth, type_='added'))
-            else:
-                lines.append(generate_line(key, val, depth, type_))
+                case {'type': 'changed', 'value': old_v, 'new_value': new_v}:
+                    lines.append(generate_line(k, old_v, depth, symbol='-'))
+                    lines.append(generate_line(k, new_v, depth, symbol='+'))
+
+                case {'type': 'nested'} | {'type': 'unchanged'}:
+                    value = key_info['value']
+                    lines.append(generate_line(k, value, depth, symbol=' '))
+
+                case _:
+                    lines.append(generate_line(k, key_info, depth, symbol=' '))
 
         closing_bracket = f'{INDENT * depth}}}'
         lines = '\n'.join(lines)
         return f'{{\n{lines}\n{closing_bracket}'
 
-    def generate_line(key: str, value, depth: int, type_=None) -> str:
+    def generate_line(key, value, depth, symbol):
         new_depth = depth + 1
         deep_indent = (INDENT * new_depth)[:-2]
-
-        match type_:
-            case 'added':
-                symbol = '+'
-            case 'deleted':
-                symbol = '-'
-            case _:
-                symbol = ' '
-
         return f'{deep_indent}{symbol} {key}: {stylish(value, new_depth)}'
 
     return stylish(diff, depth=0)

c3e448553efb01a9deb536e31b4ffea9c5403a91
Sergei <reyangood@gmail.com>
2023-12-07 16:45:29 +0300

    delete old fixtures

--- tests/fixtures/test_files.py
+++ tests/fixtures/test_files.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def get_file_paths(f1_name, f2_name):
     e1, e2 = get_file_ext(f1_name), get_file_ext(f2_name)
     return f'tests/fixtures/{f1_name}.{e1}', f'tests/fixtures/{f2_name}.{e2}'
@@ -14,28 +11,3 @@ def get_file_ext(file_name):
     else:
         ext = 'json'
     return ext
-
-
-@pytest.fixture
-def file1_json():
-    return 'tests/fixtures/flat1.json'
-
-
-@pytest.fixture
-def file2_json():
-    return 'tests/fixtures/flat2.json'
-
-
-@pytest.fixture
-def nested_file1_json():
-    return 'tests/fixtures/nested1.json'
-
-
-@pytest.fixture
-def nested_file2_json():
-    return 'tests/fixtures/nested2.json'
-
-
-@pytest.fixture
-def nested_file2_yml():
-    return 'tests/fixtures/nested2_yml.yml'

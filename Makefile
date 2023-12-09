d1474da52811fd5c2994c0013ff1d62199c1a1ed
Sergei <reyangood@gmail.com>
2023-12-09 19:43:58 +0300

    code refactoring in tests

--- Makefile
+++ Makefile
@@ -1,22 +1,31 @@
 install:
 	poetry install
+
 gendiff:
 	poetry run gendiff
+
 lint:
 	poetry run flake8 gendiff
+
 test:
 	poetry run pytest -vvvv
+
 check:
 	make lint
 	make test
+
 test-coverage:
 	poetry run pytest --cov --cov-report term-missing --cov-report xml
+
 build:
 	poetry build
+
 publish:
 	poetry publish --dry-run
+
 package-install:
 	python3 -m pip install dist/*.whl --force-reinstall
+
 bpp:
 	make build
 	make publish
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,47 @@
-import pytest
-
 from gendiff import generate_diff
-
-from tests.fixtures.test_files import get_file_paths
-from tests.fixtures.result_fixtures import get_result
-
-test_params = [
-    (get_file_paths('flat1', 'flat2'), get_result('result_flat1')),
-    (get_file_paths('flat2', 'flat1'), get_result('result_flat2')),
-    (get_file_paths('nested1', 'nested2'), get_result('result_recursive')),
-    (get_file_paths('nested1', 'nested2_yml'), get_result('result_recursive')),
-]
+from gendiff.output_formats.consts import FORMATS
 
 
-@pytest.mark.parametrize('file_paths,expected', test_params)
-def test_gendiff(file_paths, expected):
-    assert generate_diff(*file_paths) == expected
+def get_file_paths(f1_name, f2_name):
+    return f'tests/fixtures/{f1_name}.json', f'tests/fixtures/{f2_name}.json'
+
+
+def get_result(result_name):
+    with open(f'tests/fixtures/results/{result_name}') as file:
+        return file.read()
+
+
+result_flat_stylish = get_result('result_flat_stylish')
+result_flat_plain = get_result('result_flat_plain')
+result_flat_json = get_result('result_flat_json')
+
+
+def test_gendiff_flat():
+    file_paths = get_file_paths('flat1', 'flat2')
+    assert generate_diff(*file_paths) == result_flat_stylish
+    assert generate_diff(*file_paths, FORMATS.STYLISH) == result_flat_stylish
+    assert generate_diff(*file_paths, FORMATS.PLAIN) == result_flat_plain
+    assert generate_diff(*file_paths, FORMATS.JSON) == result_flat_json
+
+
+result_nested_stylish = get_result('result_nested_stylish')
+result_nested_plain = get_result('result_nested_plain')
+result_nested_json = get_result('result_nested_json')
+
+
+def test_gendiff_nested():
+    file_paths = get_file_paths('nested1', 'nested2')
+    assert generate_diff(*file_paths, FORMATS.STYLISH) == result_nested_stylish
+    assert generate_diff(*file_paths, FORMATS.PLAIN) == result_nested_plain
+    assert generate_diff(*file_paths, FORMATS.JSON) == result_nested_json
+
+
+def test_gendiff_yml():
+    file_paths = f'tests/fixtures/nested1.json', f'tests/fixtures/nested2.yml'
+    assert generate_diff(*file_paths) == result_nested_stylish
+
+
+def test_wrong_format():
+    file_paths = get_file_paths('nested1', 'nested2')
+    result = 'Unsupported format!\nPlease choose from stylish, plain, json'
+    assert generate_diff(*file_paths, format_name='avada_kedavra') == result

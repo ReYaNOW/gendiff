e8d38f92d13a43dc2b851cc17da5c0af42a8da7b
Sergei <reyangood@gmail.com>
2023-12-14 00:15:08 +0300

    add an explanatory comment to the file

--- gendiff/output_formats/consts.py
+++ gendiff/output_formats/consts.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-
+# if you want to add new_format, add new name in _FORMAT_VALUES and that's it
 _FORMAT_VALUES = ('stylish', 'plain', 'json')
 FORMATS = namedtuple('FormatChoices', map(str.upper, _FORMAT_VALUES))(
     *_FORMAT_VALUES
